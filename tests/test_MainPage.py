d52d945dc0b448199672725aae5283e832d30d30
S3M30W <100037576+S3M30W@users.noreply.github.com>
2022-08-17 22:50:52 +0300

    Update test_MainPage.py

--- tests/test_MainPage.py
+++ tests/test_MainPage.py
@@ -1,1 +1,62 @@
+import pytest
+from pages.Main_page import MainPage
+from config import TestData
+from time import sleep
+
+
+# Логотип Ozon виден на странице
+def test_main_logo_on_page(driver):
+    main_page = MainPage(driver)
+    logo = main_page.is_visible(MainPage.MAIN_LOGO)
+    assert logo == True
+
+
+# Попадаем в раздел Выгодные предложения при клике по лого Ozon
+# def test_logo_ozon_clickable(driver):
+# main_page = MainPage(driver)
+# main_page.find_click(MainPage.MAIN_LOGO)
+# title_of_window = main_page.get_windows_title(TestData.HEADER_OF_WINDOW_MAIN_LOGO)
+# assert title_of_window == True
+
+
+# Кнопка "Каталог" видна на странице
+def test_button_catalog_on_page(driver):
+    main_page = MainPage(driver)
+    button = main_page.is_visible(MainPage.CATALOG)
+    assert button == True
+
+
+# Кнопка "Каталог" имеет верное название
+def test_button_catalog_has_true_name(driver):
+    main_page = MainPage(driver)
+    button_name = main_page.get_text_of_element(MainPage.CATALOG)
+    assert button_name == TestData.NAME_OF_BUTTON_CATALOG
+
+
+# Кнопка локализации поиска видна на странице
+def test_search_where_on_page(driver):
+    main_page = MainPage(driver)
+    button = main_page.is_visible(MainPage.SEARCH_WHERE)
+    assert button == True
+
+
+# Строка поиска видна на странице
+def test_search_on_page(driver):
+    main_page = MainPage(driver)
+    field_input = main_page.is_visible(MainPage.SEARCH_INPUT)
+    assert field_input == True
+
+
+# Кнопка подтверждения ввода в строку поиска видна на странице
+def test_submit_search_on_page(driver):
+    main_page = MainPage(driver)
+    submit_search = main_page.is_visible(MainPage.SUBMIT_SEARCH)
+    assert submit_search == True
+
+
+# Кнопка Войти видна на странице
+def test_entrance_on_page(driver):
+    main_page = MainPage(driver)
+    entrance = main_page.is_visible(MainPage.ENTRANCE)
+    assert entrance == True
 
